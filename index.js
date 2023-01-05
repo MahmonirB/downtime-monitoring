2faa3ef510ab2d632bdc17e60cb256947302b0dd
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-05 11:46:25 +0100

    feat: Add node cron/improve port

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 
 app.use('/', home);
 
-app.listen(3000, () => console.log(`listen to server port: ${3000} ...`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listen to server port: ${port} ...`));
