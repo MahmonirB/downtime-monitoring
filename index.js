8f4d8303a6ef42ff437db817c2b1234d9f3c62c7
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-05 21:13:40 +0100

    feat: Add mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 require('./startups/routers')(app);
+require('./startups/db');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listen to server port: ${port} ...`));
---
+++ startups/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+module.exports = function() {
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect("mongodb://localhost/downTime-monitoring")
+  .then((res) => console.log(res.message));
+
+}
