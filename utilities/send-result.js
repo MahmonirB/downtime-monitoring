f89d4c5b5559748358c8bb929757f39083f64594
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-01-06 15:50:48 +0100

    feat: send a SMTP email/ add config

--- utilities/send-result.js
+++ utilities/send-result.js
@@ -1,5 +1,6 @@
 const checkWebsite = require("./check-website");
 const saveDownlog = require("./save-downlog");
+const sendEmail = require('./email');
 
 module.exports = async function sendResult(url) {
     const result = await checkWebsite(url);
@@ -9,5 +10,6 @@ module.exports = async function sendResult(url) {
     } else {
       console.log(`${url} is down!`);
       saveDownlog(url);
+      sendEmail(email, url);
     }
   }
